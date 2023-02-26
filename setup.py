838e66ed73f6ae3a316c0ed1a3e8e25ee84ec4e1
johnypark <parkjohnyc@gmail.com>
2023-02-25 23:56:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vit-tensorflow", # Replace with your own username
+    version="na",
+    author="Kim",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnypark/vit-tensorflow",
+    packages=setuptools.find_packages(),
+    install_requires = ['tensorflow',
+                       'tensorflow-addons',
+                        'typeguard'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
